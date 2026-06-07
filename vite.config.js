b1f356a2e3442576cbf6c129b04f5891cb020361
harry <hdpater@noreply.com>
2026-06-07 14:27:34 +0100

    corrected base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/birthday-quest/',
   build: {
     outDir: 'dist',
     chunkSizeWarningLimit: 4000,
